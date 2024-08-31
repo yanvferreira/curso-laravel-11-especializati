f3366d8873f6961be5b68a48af3a979af744929a
Yan Ferreira <yanfvc@gmail.com>
2024-08-31 17:04:21 -0300

    Criado login e o primeiro model com Artisan na aula 06 do curso de Laravel 11.

---
+++ app/Models/Categoria.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    use HasFactory;
+
+    // Caso precise mudar o nome da tabela para usar em sistemas legados
+    // protected $table = 'categories';
+}
